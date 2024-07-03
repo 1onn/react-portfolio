435b40bb0bc039a2ee71178fa18ce92ecefb7e03
Hammad Ahmad <hammadahmad9999@hotmail.com>
2024-07-04 03:30:54 +0500

    second commit

---
+++ src/data/constants.js
@@ -0,0 +1,357 @@
+import logo from "../images/logo.png";
+
+export const Bio = {
+  name: "Hammad Ahmad",
+  roles: [
+    "Full Stack Developer",
+    "Data Scientist",
+    "Software Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/1onn",
+  linkedin: "https://www.linkedin.com/in/hammad-ahmad-408103241/",
+  kaggle: "https://www.kaggle.com/hammadahmad9999",
+  contact: {
+    phone: "+92-311-5749405",
+    email: "hammadahmad9999@hotmail.com",
+    address: "Media Town, Block B, Islamabad, Pakistan, 44000",
+  },
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image: "https://mui.com/static/logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image: "https://expressjs.com/images/express-facebook-share.png",
+      },
+      {
+        name: "Graph Ql",
+        image: "https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  // {
+  //   title: "DevOps",
+  //   skills: [
+  //     {
+  //       name: "AWS",
+  //       image:
+  //         "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+  //     },
+  //     {
+  //       name: "Google Cloud",
+  //       image:
+  //         "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+  //     },
+  //     {
+  //       name: "Docker",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "Jenkins",
+  //       image:
+  //         "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+  //     },
+  //     {
+  //       name: "Nginx",
+  //       image: "https://download.logo.wine/logo/Nginx/Nginx-Logo.wine.png",
+  //     },
+  //     {
+  //       name: "Grafana",
+  //       image:
+  //         "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Grafana_logo.svg/1200px-Grafana_logo.svg.png",
+  //     },
+  //     {
+  //       name: "Kubernetes",
+  //       image:
+  //         "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+  //     },
+  //     {
+  //       name: "Prometheus",
+  //       image:
+  //         "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Prometheus_software_logo.svg/1200px-Prometheus_software_logo.svg.png",
+  //     },
+  //   ],
+  // },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "Kotlin",
+  //       image:
+  //         "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  //     },
+  //     {
+  //       name: "Jetpack Compose",
+  //       image:
+  //         "https://3.bp.blogspot.com/-VVp3WvJvl84/X0Vu6EjYqDI/AAAAAAAAPjU/ZOMKiUlgfg8ok8DY8Hc-ocOvGdB0z86AgCLcBGAsYHQ/s1600/jetpack%2Bcompose%2Bicon_RGB.png",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  {
+    title: "Data Science & Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Google_Colaboratory_SVG_Logo.svg/1200px-Google_Colaboratory_SVG_Logo.svg.png",
+      },
+      {
+        name: "Sk Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+      {
+        name: "Matplotlib",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/84/Matplotlib_icon.svg",
+      },
+      {
+        name: "Seaborn",
+        image: "https://seaborn.pydata.org/_static/logo-wide-lightbg.svg",
+      },
+      {
+        name: "Pandas",
+        image: "https://pandas.pydata.org/static/img/pandas_mark.svg",
+      },
+      {
+        name: "NumPy",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/31/NumPy_logo_2020.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Vercel",
+        image:
+          "https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://logos-world.net/wp-content/uploads/2020/02/Canva-Logo.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: logo,
+    role: "Research Assistant",
+    company: "COMSATS University Islamabad",
+    date: "Jan 2024 - June 2024",
+    desc: "Led comprehensive data science and analytics research to identify key health indicators for diabetes and develop a predictive model with the best accuracy. Transformed the predictive model into a web application using React for the front end and Flask for the back end, which predicts if the patient is prone to diabetes. If not, it provides the percentage risk and also highlights the contributing factor to help people stay safe.",
+    skills: [
+      "Python",
+      "React",
+      "Flask",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Scikit-Learn",
+      "Pandas",
+      "NumPy",
+      "Matplotlib",
+      "Joblib",
+    ],
+  },
+  {
+    id: 1,
+    img: logo,
+    role: "Data Science Intern",
+    company: "COMSATS University Islamabad",
+    date: "July 2023 - Sep 2023",
+    desc: "Utilized data science and data analytics to build a model for sleep efficiency. Developed a comprehensive research paper titled ”Comparative Analysis of Machine Learning Methods for Enhancing Sleep Efficiency and Prediction”. Collaborated with senior researchers to enhance the accuracy and performance of the predictive model.",
+    skills: ["Python", "Scikit-Learn", "Pandas", "NumPy", "Matplotlib"],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: logo,
+    school: "COMSATS University Islamabad, Pakistan",
+    date: "2020 - 2024",
+    grade: "Bachelor of Science in Bioinformatics",
+    desc: "I am currently pursuing a Bachelor's degree in Bioinformatics at COMSATS University Islamabad. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Science in Bioinformatics",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "React-Keeper-App",
+    date: "2023",
+    description:
+      "Created a clone of Google Keep, demonstrating my expertise in web development. Replicates the functionality of Google Keep, allowing users to manage notes effectively.",
+    image:
+      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+    tags: ["React", "Node.js", "Express.js", "MongoDB"],
+    category: "web app",
+    github: "https://github.com/hammadahmad9999/React-Keeper-App",
+  },
+  {
+    id: 9,
+    title: "AI-Based Sleep Efficiency Predictor Tool",
+    date: "2023",
+    description:
+      "Developed an AI-based modern tool to predict sleep efficiency. Integrated with Random Forest Model and converted into a web app using Flask framework. Enhanced user experience using HTML and CSS for the front end.",
+    image:
+      "https://github.com/hammadahmad9999/AI-Based-Sleep-Efficiency-Predictor-Tool/raw/master/assets/sleep_efficiency.jpg",
+    tags: ["Python", "Flask", "HTML", "Bootstrap"],
+    category: "web app",
+    github:
+      "https://github.com/hammadahmad9999/AI-Based-Sleep-Efficiency-Predictor-Tool",
+    webapp: "https://sleep-efficiency-predictor-tool.netlify.app/",
+  },
+];
+
+export const publications = [
+  {
+    id: 0,
+    title:
+      "Comparative Analysis of Machine Learning Methods for Enhancing Sleep Efficiency",
+    date: "May 2024",
+    authors: "Hammad Ahmad, Umar Khan, Maleeha Azam",
+    conference: "ICSMAI Conference 2024, Morocco",
+    description:
+      "Presented a comprehensive analysis of sleep efficiency by evaluating various influential factors. Used machine learning algorithms including Linear Regression, Decision Tree, Random Forest, and Gradient Boosting Regressor.",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+];
